6a89220b6b57973695619216c7df38836ca4fbfd
Hugo Wood <hx4.5@free.fr>
2017-02-19 15:16:26 +0100

    docs(grid-utils): jsdoc with types

--- grid-utils.js
+++ grid-utils.js
@@ -1,22 +1,56 @@
 const _ = require('lodash')
 
+/**
+ * @typedef {Array.<Array>} Grid
+ */
+
+/**
+ * @typedef {Object} Cell
+ * @property value
+ * @property {number} x
+ * @property {number} y
+ */
+
+/**
+ * @callback GridIteratee
+ * @param value
+ * @param {number} x
+ * @param {number} y
+ * @param {Grid} grid
+ * @param {Array} row
+ */
+
 module.exports = {
+
   /**
-   * @returns a flatten array of all cells in the grid (cell is object with props x, y, value).
+   * @param {Grid} grid
+   * @returns {Array.<Cell>}
    */
   cells: lift(_.flatMap, _.map, (value, x, y) => ({value, x, y})),
+
   /**
-   * Runs a function on each cell. Function takes params value, x, y, grid, row.
+   * Runs a function on each cell.
+   * @param {Grid} grid
+   * @param {GridIteratee} f
+   * @returns {undefined}
    */
   each: lift(_.each, _.each, (value, x, y, grid, row, f) => f(value, x, y, grid, row)),
+
   /**
-   * Maps values of the cells. Function takes same params as `each`.
+   * Maps values of the cells.
+   * @param {Grid} grid
+   * @param {GridIteratee} f
+   * @returns {Grid}
    */
   map: lift(_.map, _.map, (value, x, y, grid, row, f) => f(value, x, y, grid, row)),
+
   /**
    * Matrix transposition from maths. Inverts columns and rows. https://en.wikipedia.org/wiki/Transpose
+   * @param {Grid} grid
+   * @returns {Grid}
    */
   transpose
+
 }
 
 function lift (fgrid, frow, fvalue) {
