fdf265d985965818b475e3f21ecdda26dc251fd3
Hugo Wood <hx4.5@free.fr>
2017-02-23 20:18:51 +0100

    feat: sorted by popularity * latency saved

--- solve.js
+++ solve.js
@@ -2,28 +2,29 @@ const debug = require('debug')('solve')
 const _ = require('lodash')
 const gridUtils = require('./grid-utils')
 const getCacheServersByEndpoint = require('./get-cache-servers-by-endpoint')
+const requestsSortedByLatencySaved = require('./requests-sorted-by-latency-saved')
 
 module.exports = function solve (problem) {
-  const requestByPopularity = _.orderBy(problem.requests, ['popularity'], ['desc'])
+  const requestByPopularity = requestsSortedByLatencySaved(problem) //_.orderBy(problem.requests, ['popularity'], ['desc'])
 
   const cacheServers = _.uniqBy(_.map(_.flatMap(problem.endpoints, endpoint => endpoint.cacheServers), ({id}) => ({id, capacity: problem.cacheServerCapacity})), 'id')
 
   let requestsWithCacheServer = _.map(requestByPopularity, request => {
     const video = _.find(problem.videos, {index: request.video})
 
-    let cacheServersByEndPoint = getCacheServersByEndpoint(problem, request.endpoint)
-    cacheServersByEndPoint = _.filter(cacheServersByEndPoint, ({id}) => {
+    let cacheServersByEndPoint = [request.cache] // getCacheServersByEndpoint(problem, request.endpoint)
+    cacheServersByEndPoint = _.filter(cacheServersByEndPoint, (id) => {
       const cacheServer = _.find(cacheServers, {id})
       return cacheServer && cacheServer.capacity >= video.size
     })
-    cacheServersByEndPoint = _.sortBy(cacheServersByEndPoint, 'latency')
+    // cacheServersByEndPoint = _.sortBy(cacheServersByEndPoint, 'latency')
 
     const cacheServer = _.first(cacheServersByEndPoint)
     if (cacheServer) {
-      _.find(cacheServers, {id: cacheServer.id}).capacity -= video.size
+      _.find(cacheServers, {id: cacheServer}).capacity -= video.size
       return {
         request,
-        cacheServer
+        cacheServer: {id: cacheServer}
       }
     }
   })
