83a78aa6f28a65ef5e7f409aafbf05e82d7edeab
Hugo Wood <hx4.5@free.fr>
2017-02-23 20:37:54 +0100

    fix: removed links where saved latency is less than zero

--- requests-sorted-by-latency-saved.js
+++ requests-sorted-by-latency-saved.js
@@ -5,12 +5,12 @@ const _ = require('lodash')
 module.exports = function requestsSortedByLatencySaved (problem) {
   return _.orderBy(_.reduce(problem.requests, (links, request) => {
     const endpoint = problem.endpoints[request.endpoint]
-    return links.concat(_.map(endpoint.cacheServers, cache => ({
+    return links.concat(_(endpoint.cacheServers.map(cache => ({
       video: request.video,
       endpoint: endpoint.index,
       cache: cache.id,
       latency: request.popularity * cache.latency,
       savedLatency: request.popularity * endpoint.datacenterLatency - request.popularity * cache.latency
-    })))
+    })).filter(link => link.savedLatency > 0)).value())
   }, []), ['savedLatency'], ['desc'])
 }
