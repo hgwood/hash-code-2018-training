9d604ed3a8b059448fe2f33e89ffa563415578eb
Hugo Wood <hx4.5@free.fr>
2017-02-23 19:22:21 +0100

    feat: read

--- read.js
+++ read.js
@@ -11,7 +11,15 @@ module.exports = function read (filePath) {
 
 function parse (textFromInputFile) {
   const parse = jolicitron((save, n) => [
-    // TODO: insert parser config here
+    save('nvideos'),
+    save('nendpoints'),
+    save('nrequestDescriptions'),
+    save('ncacheServers'),
+    'cacheServerCapacity',
+    n('videos', {length: 'nvideos', indices: true}, 'size'),
+    n('endpoints', {length: 'nendpoints', indices: true}, 'datacenterLatency', save('navailableCaches'),
+      n('cacheServers', {length: 'navailableCaches'}, 'id', 'latency')),
+    n('requests', {length: 'nrequestDescriptions', indices: true}, 'video', 'endpoint', 'popularity')
   ])
   const {parsedValue, remaining} = parse(textFromInputFile)
   assert.equal(remaining.trim(), '')
