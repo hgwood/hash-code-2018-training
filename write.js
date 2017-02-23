70d9b90f28f3b243eafc1430238fcad583876a7c
nlepage <nicolas.lepage@zenika.com>
2017-02-23 19:02:29 +0100

    write

--- write.js
+++ write.js
@@ -10,10 +10,25 @@ function writeLines (path, lines) {
   fs.writeFileSync(path, lines.join('\n'))
   debug(`wrote ${lines.length} lines to ${path}`)
 }
+
+// Solution model :
+// solution = [
+//   {
+//     id: 1,
+//     videos: [1, 2, 3, 4]
+//   },
+//   ...
+// ]
+
 function unparse (solution) {
-  // TODO: insert write logic here
-  // must return an array of lines to write to the output file
-  return []
+  const cacheServers = _.filter(solution, cacheServer => !_.isEmpty(cacheServer.videos)) // Filter empty servers
+  return [
+    `${cacheServers.length}`,
+    ..._.map(
+      cacheServers,
+      cacheServer => `${cacheServer.id} ${_.join(cacheServer.videos, ' ')}`
+    )
+  ]
 }
 
 module.exports.unparse = unparse
--- write.test.js
+++ write.test.js
@@ -6,10 +6,23 @@ const unparse = require('./write').unparse
 describe('unparse', function () {
   it('unparses correctly', function () {
     assert.deepEqual(
-      unparse({
-        // TODO: insert input here
-      }), [
-        // TODO: insert expected result here
+      unparse([
+        {
+          id: 1,
+          videos: [1, 2, 3, 4]
+        },
+        {
+          id: 2,
+          videos: []
+        },
+        {
+          id: 3,
+          videos: [9, 8]
+        }
+      ]), [
+        '2',
+        '1 1 2 3 4',
+        '3 9 8'
       ])
   })
 })
