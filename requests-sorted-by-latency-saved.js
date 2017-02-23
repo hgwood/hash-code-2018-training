aa1a32685ce67dd4b935c3cf878e22e5f273c83d
Hugo Wood <hx4.5@free.fr>
2017-02-23 20:53:45 +0100

    feat: support for current latency

--- requests-sorted-by-latency-saved.js
+++ requests-sorted-by-latency-saved.js
@@ -2,15 +2,16 @@ const assert = require('assert')
 const debug = require('debug')('requestsSortedByLatencySaved')
 const _ = require('lodash')
 
-module.exports = function requestsSortedByLatencySaved (problem) {
+module.exports = function requestsSortedByLatencySaved (problem, latencies) {
   return _.orderBy(_.reduce(problem.requests, (links, request) => {
     const endpoint = problem.endpoints[request.endpoint]
+    const latency = _.get(latencies, [endpoint.index, request.video], endpoint.datacenterLatency)
     return links.concat(_(endpoint.cacheServers.map(cache => ({
       video: request.video,
       endpoint: endpoint.index,
       cache: cache.id,
       latency: request.popularity * cache.latency,
-      savedLatency: request.popularity * endpoint.datacenterLatency - request.popularity * cache.latency
+      savedLatency: request.popularity * (latency - cache.latency)
     })).filter(link => link.savedLatency > 0)).value())
   }, []), ['savedLatency'], ['desc'])
 }
