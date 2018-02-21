d03b1ec53d09d6745a52eecceecab427af876f38
Hugo Wood <hx4.5@free.fr>
2018-02-21 22:04:15 +0100

    reset solve

--- solve.js
+++ solve.js
@@ -1,50 +1,7 @@
 const debug = require('debug')('solve')
 const _ = require('lodash')
 const gridUtils = require('./grid-utils')
-const requestsSortedByLatencySaved = require('./requests-sorted-by-latency-saved')
 
 module.exports = function solve (problem) {
-  debug('nvideos', problem.videos.length)
-  debug('nendpoints', problem.endpoints.length)
-  debug('nrequests', problem.requests.length)
 
-  debug('cacheServers start')
-  const cacheServers = _.uniqBy(_.map(_.flatMap(problem.endpoints, endpoint => endpoint.cacheServers), ({id}) => ({id, capacity: problem.cacheServerCapacity})), 'id')
-  debug('cacheServers end')
-
-  debug('videosLatencyByEnpoint start')
-  const videosLatencyByEnpoint = _.mapValues(
-    _.groupBy(problem.requests, 'endpoint'),
-    requests => _.mapValues(
-      _.keyBy(requests, 'video'),
-      request => _.find(problem.endpoints, { index: request.endpoint }).datacenterLatency
-    )
-  )
-
-  let requestsWithCacheServer = _.map(requestsSortedByLatencySaved(problem, videosLatencyByEnpoint), request => {
-    const video = _.find(problem.videos, {index: request.video})
-
-    let cacheServersByEndPoint = [request.cache] // getCacheServersByEndpoint(problem, request.endpoint)
-    cacheServersByEndPoint = _.filter(cacheServersByEndPoint, (id) => {
-      const cacheServer = _.find(cacheServers, {id})
-      return cacheServer && cacheServer.capacity >= video.size
-    })
-    // cacheServersByEndPoint = _.sortBy(cacheServersByEndPoint, 'latency')
-
-    const cacheServer = _.first(cacheServersByEndPoint)
-    if (cacheServer !== undefined) {
-      _.find(cacheServers, {id: cacheServer}).capacity -= video.size
-      return {
-        request,
-        cacheServer: {id: cacheServer}
-      }
-    }
-  })
-  requestsWithCacheServer = _.compact(requestsWithCacheServer)
-  const requestsByCacheServer = _.groupBy(requestsWithCacheServer, 'cacheServer.id')
-  const solution = _.map(requestsByCacheServer, (requests, id) => ({
-    id,
-    videos: _.uniq(_.map(requests, request => request.request.video))
-  }))
-  return solution
 }
