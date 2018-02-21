4a63516aac6a992e32c801797efac64d920cfc2c
Hugo Wood <hx4.5@free.fr>
2018-02-21 22:01:02 +0100

    Revert "write"
    This reverts commit 70d9b90f28f3b243eafc1430238fcad583876a7c.

--- write.test.js
+++ write.test.js
@@ -6,23 +6,10 @@ const unparse = require('./write').unparse
 describe('unparse', function () {
   it('unparses correctly', function () {
     assert.deepEqual(
-      unparse([
-        {
-          id: 1,
-          videos: [1, 2, 3, 4]
-        },
-        {
-          id: 2,
-          videos: []
-        },
-        {
-          id: 3,
-          videos: [9, 8]
-        }
-      ]), [
-        '2',
-        '1 1 2 3 4',
-        '3 9 8'
+      unparse({
+        // TODO: insert input here
+      }), [
+        // TODO: insert expected result here
       ])
   })
 })
