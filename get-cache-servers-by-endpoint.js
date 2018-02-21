7bfd7fbfe852a6e5b0411cd6eca56b7e253eb6ca
Hugo Wood <hx4.5@free.fr>
2018-02-21 22:00:33 +0100

    :fire: remove inputs and specific files from 2017

--- get-cache-servers-by-endpoint.js
+++
@@ -1,7 +0,0 @@
-const assert = require('assert')
-const debug = require('debug')('getCacheServersByEndpoint')
-const _ = require('lodash')
-
-module.exports = function getCacheServersByEndpoint (problem, index) {
-  return _.find(problem.endpoints, { index }).cacheServers
-}
