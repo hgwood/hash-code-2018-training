d62f665522c8dcbe59417c0ed0dd1412bd45b5e7
Hugo Wood <hx4.5@free.fr>
2018-02-24 00:40:23 +0100

    :white_check_mark: read test

--- read.test.js
+++ read.test.js
@@ -5,9 +5,21 @@ const { parse } = require("./read");
 
 describe("parse", function() {
   it("parses correctly", function() {
-    const textFromInputFile = ``; // TODO: insert test input here
+    const textFromInputFile = `3 5 1 6
+    TTTTT
+    TMMMT
+    TTTTT
+    `;
     assert.deepEqual(parse(textFromInputFile), {
-      // TODO: insert expected output here
+      nrows: 3,
+      ncolumns: 5,
+      minIngredients: 1,
+      maxCells: 6,
+      pizza: [
+        ["T", "T", "T", "T", "T"],
+        ["T", "M", "M", "M", "T"],
+        ["T", "T", "T", "T", "T"]
+      ]
     });
   });
 });
