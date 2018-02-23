8228fd7f64fe7328d7dbe366ea410256bee59b14
Hugo Wood <hx4.5@free.fr>
2018-02-24 00:41:03 +0100

    :bulb: jsdoc for solve fn

--- solve.js
+++ solve.js
@@ -2,4 +2,23 @@ const debug = require("debug")("solve");
 const _ = require("lodash");
 const gridUtils = require("./grid-utils");
 
-module.exports = function solve(problem) {};
+/**
+ * @typedef {object} ProblemInput
+ * @property {number} nrows
+ * @property {number} ncolumns
+ * @property {number} minIngredients
+ * @property {number} maxCells
+ * @property {string[][]} pizza
+ *
+ * @typedef {object} Slice
+ * @property {number} r1
+ * @property {number} c1
+ * @property {number} r2
+ * @property {number} c2
+ *
+ * @param {ProblemInput} problem
+ * @returns {Slice[]}
+ */
+function solve(problem) {}
+
+module.exports = solve;
