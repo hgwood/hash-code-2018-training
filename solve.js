bd5223af5d2914e927492bfddf9f31975c64f8c5
Thibaud Courtoison <do.not.press.enter@gmail.com>
2018-02-26 21:10:37 +0100

    'tayo!'

--- solve.js
+++ solve.js
@@ -2,6 +2,20 @@ const debug = require("debug")("solve");
 const _ = require("lodash");
 const gridUtils = require("./grid-utils");
 
+function partValide(pizza, slice, minIngredients) {
+  let t = 0;
+  let m = 0;
+
+  _.range(slice.r1, slice.r2 + 1).forEach(y => {
+    _.range(slice.c1, slice.c2 + 1).forEach(x => {
+      if (pizza[y][x] == "T") t++;
+      if (pizza[y][x] == "M") m++;
+    });
+  });
+
+  return t >= minIngredients && m >= minIngredients;
+}
+
 /**
  * @typedef {object} ProblemInput
  * @property {number} nrows
@@ -28,6 +42,10 @@ function solve(problem) {
     problem.pizza = gridUtils.transpose(problem.pizza);
   }
 
+  const pizzaCopy = _.map(problem.pizza, line => _.map(line, x => x));
+
+  // Step 1: prendre les lines
+
   problem.pizza.forEach((line, index) => {
     _.range(0, line.length, problem.maxCells).forEach(x => {
       let ingredients = _.slice(line, x, x + problem.maxCells).reduce(
@@ -43,19 +61,67 @@ function solve(problem) {
         ingredients["M"] >= problem.minIngredients
       ) {
         if (transposed) {
-          slices.push({
+          slice = {
             r1: x,
             c1: index,
             r2: x + Math.min(line.length - x, problem.maxCells) - 1,
             c2: index
-          });
+          };
         } else {
-          slices.push({
+          slice = {
             r1: index,
             c1: x,
             r2: index,
             c2: x + Math.min(line.length - x, problem.maxCells) - 1
+          };
+        }
+
+        slices.push(slice);
+
+        const { r1, r2, c1, c2 } = slice;
+
+        _.range(r1, r2 + 1).forEach(y => {
+          _.range(c1, c2 + 1).forEach(x => {
+            pizzaCopy[y][x] = " ";
           });
+        });
+      }
+    });
+  });
+
+  // Step 2: prendre 2 colonnes
+  pizzaCopy.forEach((line, y) => {
+    _.range(0, line.length, problem.maxCells).forEach(x => {
+      if (line[x] != " ") {
+        if (y < pizzaCopy.length - 1 && pizzaCopy[y + 1][x] != " ") {
+          let finDeLigne = Math.min(
+            x + Math.floor(problem.maxCells / 2),
+            line.length
+          );
+
+          let slice1 = {
+            r1: y,
+            r2: y + 1,
+            c1: x,
+            c2: finDeLigne
+          };
+
+          if (partValide(pizzaCopy, slice1, problem.minIngredients)) {
+            slices.push(slice1);
+          }
+
+          if (finDeLigne != line.length) {
+            let slice2 = {
+              r1: y,
+              r2: y + 1,
+              c1: finDeLigne + 1,
+              c2: Math.min(x + problem.maxCells, line.length)
+            };
+
+            if (partValide(pizzaCopy, slice2, problem.minIngredients)) {
+              slices.push(slice2);
+            }
+          }
         }
       }
     });
